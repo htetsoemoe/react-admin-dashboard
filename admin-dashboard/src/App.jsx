26ade1a31d72c354bbacbdfc74e34cb981937940
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-11-11 10:28:47 +0630

    ColorModeContext and ThemeProvider

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 const App = () => {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div>
-      Hello World
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+
+    </ColorModeContext.Provider>
+
   )
 }
 
