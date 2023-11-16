f49e891c6681979a0c4225f07b17dd2fad6b313b
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-11-16 17:02:57 +0630

    finished team and contacts component

--- admin-dashboard/src/scenes/team/index.jsx
+++ admin-dashboard/src/scenes/team/index.jsx
@@ -1,11 +1,110 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { Typography, Box, useTheme } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid';
+import { tokens } from '../../theme';
+import { mockDataTeam } from '../../data/mockData';
+import Header from '../../components/Header'
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 
 const index = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell"
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left"
+    },
+    {
+      field: "phone",
+      headerName: "Phone",
+      flex: 1
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1
+    },
+    {
+      field: "access",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: ({ row: { access } }) => { // using nested object destructuring
+        return (
+          <Box
+            width="60%"
+            m="0 auth"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+              access === 'admin'
+                ? colors.greenAccent[600]
+                : access === 'manager'
+                  ? colors.greenAccent[700]
+                  : colors.greenAccent[700]
+            }
+            borderRadius="4px"
+          >
+            {access === 'admin' && <AdminPanelSettingsOutlinedIcon />}
+            {access === 'manager' && <SecurityOutlinedIcon />}
+            {access === 'user' && <LockOpenOutlinedIcon />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
+            </Typography>
+          </Box>
+        )
+      }
+    }
+  ]
   return (
-    <div>
-      <Typography variant='h3' sx={{ ml: "50px", mt: "50px" }}>Team Component</Typography>
-    </div>
+    <Box m="20px">
+      <Header title="TEAM" subtitle="Managing the team members" />
+
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none"
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none"
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[200]
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400]
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700]
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`
+          }
+        }}
+      >
+        <DataGrid rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
   )
 }
 
