8195f8c005416308cd353b9f39486ad1f12d062e
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-11-23 15:35:40 +0630

    accordion component

--- admin-dashboard/src/scenes/faq/index.jsx
+++ admin-dashboard/src/scenes/faq/index.jsx
@@ -1,11 +1,101 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { Box, Typography, useTheme } from '@mui/material'
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Header from '../../components/Header'
+import { tokens } from '../../theme'
 
 const index = () => {
+
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+
   return (
-    <div>
-      <Typography variant='h3' sx={{ ml: "50px", mt: "50px" }}>FAQ Component</Typography>
-    </div>
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Ex, voluptatibus praesentium quibusdam cupiditate rem eaque minus,
+            nesciunt maxime odit at, facilis ab officia velit ea quasi voluptates
+            suscipit animi. Molestias?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            License
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Ex, voluptatibus praesentium quibusdam cupiditate rem eaque minus,
+            nesciunt maxime odit at, facilis ab officia velit ea quasi voluptates
+            suscipit animi. Molestias?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            Pricing
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Ex, voluptatibus praesentium quibusdam cupiditate rem eaque minus,
+            nesciunt maxime odit at, facilis ab officia velit ea quasi voluptates
+            suscipit animi. Molestias?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            Community
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Ex, voluptatibus praesentium quibusdam cupiditate rem eaque minus,
+            nesciunt maxime odit at, facilis ab officia velit ea quasi voluptates
+            suscipit animi. Molestias?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            Documentation
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Ex, voluptatibus praesentium quibusdam cupiditate rem eaque minus,
+            nesciunt maxime odit at, facilis ab officia velit ea quasi voluptates
+            suscipit animi. Molestias?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
   )
 }
 
